b628884c6dbeca70c7719de66d13b32301e19486
SakshiUppoor <sakshiuppoor@gmail.com>
2019-12-05 15:55:39 +0530

    Create customer app

--- customer/urls.py
+++ customer/urls.py
@@ -1,17 +1,11 @@
 
 from django.urls import path
 from django.conf.urls import include
-from rest_framework import routers
 from customer import views
 
 app_name = 'customer'
 
-#router = routers.DefaultRouter()
-#router.register(r'users', views.UserViewSet)
-#router.register(r'groups', views.GroupViewSet)
-
 urlpatterns = [
-    #path('', include(router.urls)),
     path('register/', views.register, name="signup"),
     path('login/', views.user_login, name="user_login"),
     path('logout/', views.user_logout, name="logout"),
