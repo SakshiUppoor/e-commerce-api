b3d32c8f476d62533ae128ef328222b9d064e6b9
SakshiUppoor <sakshiuppoor@gmail.com>
2019-12-18 23:14:42 +0530

    Add session auth

--- customer/urls.py
+++ customer/urls.py
@@ -6,5 +6,8 @@ from customer import views
 app_name = 'customer'
 
 urlpatterns = [
+    path('register/', views.register, name="signup"),
+    path('login/', views.user_login, name="user_login"),
+    path('logout/', views.user_logout, name="logout"),
     path('customer/api/', include("customer.api.urls")),
 ]
